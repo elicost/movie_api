7baca056cc52c7bb678cf27c77a551c24781ac34
elicost <140678342+elicost@users.noreply.github.com>
2026-01-19 12:04:42 -0600

    Implement trials for routing, logging, error-handling functions

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
+
 const app = express();
+
+// Placeholder movie information
+let topMovies = [
+    {
+        title: 'The Fall',
+        director: 'Tarsem Singh'
+    },
+    {
+        title: 'One Battle After Another',
+        director: 'Paul Thomas Anderson'
+    },
+    {
+        title: 'Prisoners',
+        director: 'Denis Villeneuve'
+    }
+
+    // Up to ten movies here
+];
+
+// Invoke Morgan logger
+app.use(morgan('common'));
+
+// Serve `documentation.html` from public folder
+app.use(express.static('public'));
+
+// GET route for default text response
+app.get('/', (req, res) => {
+    res.send('Welcome to my Movie API!');
+});
+
+// GET route for returning JSON object
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+// Error handling function
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something isn\'t working right!');
+});
