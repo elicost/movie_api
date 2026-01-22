799904997b2652aa872ddbd3c1dfd08b77e8694f
elicost <140678342+elicost@users.noreply.github.com>
2026-01-22 13:55:20 -0600

    Implement request.url to target user-gen URL

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const http = require('http'),
     fs = require('fs');
 
 http.createServer((request, response) => {
+    let addr = request.url,
+    q = new URL(addr, 'htt://' + request.headers.host),
+    filepath = '';
+
+    if (q.pathname.includes('documentation')) {
+        filepath = (__dirname + '/documenatation.html');
+    } else {
+        filepath = 'index.html';
+    }
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello, Node!\n');
 }).listen(3000);
