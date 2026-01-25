8c12f75fbb9fdffbba15ec99c8f0befeef111094
elicost <140678342+elicost@users.noreply.github.com>
2026-01-24 21:28:42 -0600

    Create get routes

--- index.js
+++ index.js
@@ -188,6 +188,117 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Movie API!');
 });
 
+// GET route for returning list of all movies
+app.get('/movies', (req, res) => {
+    res.status(200).json(movies);
+});
+
+// GET route for returning information about single movie by title
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find( movie => movie.title === title );
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('Movie not found.')
+    }
+});
+
+// GET route for returning information about single genre by name
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find( movie => movie.genre.name === genreName ).genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('Genre not found.')
+    }
+});
+
+// GET route for returning information about single director by name
+app.get('/movies/director/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find( movie => movie.director.name === directorName ).director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('Director not found.')
+    }
+});
+
+// POST route for allowing new user to register
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser);
+    } else {
+        res.status(400).send('New user creation missing name.')
+    }
+});
+
+// PUT route for allowing existing users to update name
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('User not found.')
+    }
+});
+
+// POST route for allowing user to add movie to favorites list
+app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been addded to ${id}'s array.`);
+    } else {
+        res.status(400).send('User or movie not found.')
+    }
+});
+
+// DELETE route for allowing user to delete movie from favorites list
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle )
+        res.status(200).send(`${movieTitle} has been removed from ${id}'s array.`);
+    } else {
+        res.status(400).send('User or movie not found.')
+    }
+});
+
+// DELETE route for allowing existing user to deregister
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        users = users.filter( user => user.id != id )
+        res.status(200).send(`User ${id}'s has been deleted.`);
+    } else {
+        res.status(400).send('User not found.')
+    }
+});
+
 
 
 // Error handling function
