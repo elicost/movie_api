a99d5742208e463e9eb1670c309db705b3192743
elicost <140678342+elicost@users.noreply.github.com>
2026-02-03 17:24:28 -0600

    Create POST route for login authentication

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = 'your_jwt_secret'; // MUST match key in JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
+require('./passport'); // Local passport file
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username, // Username being encoded
+        expiresIn: '7d', // Specifies 7 day expiration
+        algorithm: 'HS256' // Algorithm used to encode JWT values
+    });
+};
+
+
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right.',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+};
