c333d107d54aae6785e25886b9568ffda209bef6
elicost <140678342+elicost@users.noreply.github.com>
2026-02-03 17:38:50 -0600

    Apple JWT auth to GET route
    For returning list of all movies

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.get('/', (req, res) => {
 });
 
 // Return list of all movies (Mongoose GET route)
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
