7de8b1e40f128e77a45f9222bda511fdc2428a71
elicost <140678342+elicost@users.noreply.github.com>
2026-02-03 19:16:34 -0600

    Add username match check

--- index.js
+++ index.js
@@ -367,6 +367,9 @@ app.post('/users', async (req, res) => {
 }*/
 
 app.put('/users/:Username', async (req, res) => {
+    if(req.user.Username !== req.params.Username){
+        return res.status(400).send('Permission denied.');
+    }
     await Users.findOneAndUpdate({ Username: req.params.Username },
         { $set: {
             Username: req.body.Username,
