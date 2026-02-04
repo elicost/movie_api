b31c65416402538f3a9f3495eef6586e9c49abbf
elicost <140678342+elicost@users.noreply.github.com>
2026-02-04 10:55:01 -0600

    Add optional origin limitation function

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.use(express.urlencoded({ extended: true}));
 const cors = require('cors');
 app.use(cors());
 
+// Replace app.use(cors()); with following to allow only certain origins:
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1){
+//             let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
