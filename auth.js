bcdb10bce9c9c00c95e79e2031f534d028911a6a
elicost <140678342+elicost@users.noreply.github.com>
2026-02-07 13:28:09 -0600

    Temp debugging change

--- auth.js
+++ auth.js
@@ -20,7 +20,9 @@ module.exports = (router) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right.',
-                    user: user
+                    user: user,
+                    info: info,
+                    error: error ? error.message : null
                 });
             }
             req.login(user, { session: false }, (error) => {
