c9911e7f237dde4cbf073b76c4265abb072f1b55
elicost <140678342+elicost@users.noreply.github.com>
2026-02-07 13:41:04 -0600

    Debugging

--- passport.js
+++ passport.js
@@ -42,7 +42,7 @@ passport.use(
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JTW_SECRET
+    secretOrKey: process.env.JWT_SECRET || 'fallback_secret_for_debugging'
     }, async (jwtPayload, callback) => {
         return await Users.findById(jwtPayload._id)
             .then((user) => {
